19250b6a7e480831e3d130183b115818f606b1b9
Yann Guenver <sabrac72@gmail.com>
2016-11-23 18:56:59 +0100

    Update pipe_recepteur.c

--- Semestre 1/PSE/TP_Tubes_MSG/pipe_recepteur.c
+++ Semestre 1/PSE/TP_Tubes_MSG/pipe_recepteur.c
@@ -18,9 +18,17 @@ int
 main( int nb_arg , char * tab_arg[])
 {
      char nomprog[128] ;
+     char message[MESSAGES_TAILLE] ;
+
+    int fd_tube;
+    int i;
+
+    struct timeval temps ;
+    double temps_debut;
+    double temps_fin;
 
      /*-----*/
-     strcpy( nomprog , tab_arg[0] );
+     
 
      if( nb_arg != 1 )
      {
@@ -28,11 +36,45 @@ main( int nb_arg , char * tab_arg[])
 	  fprintf( stderr , "usage : %s \n" , nomprog );
 	  exit(-1);
      }
+     strcpy( nomprog , tab_arg[0] );
+
+     /* Ouverture du tube nomme en lecture seule */
+    if( (fd_tube = open( TUBE_NOMME, O_RDONLY, 0 )) == -1 )
+    {
+        perror("Probleme durant l'ouverture du tube nomme");
+        exit(-1);
+    }
+
+    fprintf(stderr, "\n---------- Debut reception %s ----------\n" , nomprog ) ;
+
+    /* Recuperation de l'heure actuelle avec precision de l’ordre de la microseconde */
+    gettimeofday(&temps, NULL);
+    temps_debut = temps.tv_sec+(temps.tv_usec/1000000.0);
+
+    /* Lecture des MESSAGES_NB messages dans le tube nomme */	
+    for(i=0; i<MESSAGES_NB; i++)
+    {
+        read( fd_tube, &message, MESSAGES_TAILLE);
+    }
+
+    /* Recuperation de l'heure actuelle avec precision de l’ordre de la microseconde */
+    gettimeofday(&temps, NULL);
+    temps_fin = temps.tv_sec+(temps.tv_usec/1000000.0);
 
 
-     /*=============================*/
-     /*           A FAIRE           */
-     /*=============================*/
+    fprintf(stderr, "\nTemps de reception           =  %.6lf secondes", temps_fin - temps_debut);
+    fprintf(stderr, "\nTemps de reception / message =  %.6lf secondes\n", (temps_fin - temps_debut) / MESSAGES_NB);
+    fprintf(stderr, "\n----------- Fin reception %s -----------\n\n" , nomprog ) ;
+
+    fprintf(stderr, "=== Affichage message : =======================\n");
+    pipe_afficher( message );
+    fprintf(stderr, "===============================================\n\n");
+
+    /* Fermeture du tube nomme */
+    close( fd_tube );
+
+    /* Suppression du tube nomme */
+    remove( TUBE_NOMME );
 
      exit( 0 );
 }
